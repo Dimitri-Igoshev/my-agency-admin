972cf6f98b39c14377c2c7af61e2e5194d86075a
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-08-17 15:37:28 +0200

    404

---
+++ src/pages/Page404.tsx
@@ -0,0 +1,9 @@
+import { AuthLayout } from '../layouts/AuthLayout'
+
+export const Page404 = () => {
+	return (
+		<AuthLayout>
+			<div className="text-white text-[100px] font-bold">404</div>
+		</AuthLayout>
+	)
+}
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,6 +4,7 @@ import { BaseLayout } from '../layouts/BaseLayout'
 import { Dashboard } from '../pages/Dashboard'
 import { Auth } from '../pages/Auth'
 import { Login } from '../widgets/Login'
+import { Page404 } from '../pages/Page404'
 
 export const AppRoutes = () => {
   return (
@@ -13,6 +14,7 @@ export const AppRoutes = () => {
         <Route index element={<Login />} />
         <Route path="login" element={<Login />} />
       </Route>
+      <Route path="*" element={<Page404/>} />
     </Routes>
   )
 }
