e1d8be0f16be729688f33f7c8c45689dc4f5d14d
Dimitri-Igoshev <d.o.igoshev@gmail.com>
2023-08-17 15:46:06 +0200

    routing

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,18 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import { AuthLayout } from '../layouts/AuthLayout'
 import { BaseLayout } from '../layouts/BaseLayout'
 import { Dashboard } from '../pages/Dashboard'
-import { Auth } from '../pages/Auth'
 import { Login } from '../widgets/Login'
 import { Page404 } from '../pages/Page404'
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<BaseLayout><Dashboard /></BaseLayout>} />
-      <Route path="auth" element={<AuthLayout><Auth /></AuthLayout>} >
-        <Route index element={<Login />} />
-        <Route path="login" element={<Login />} />
-      </Route>
+      <Route path="/" element={<AuthLayout><Login /></AuthLayout>} />
+      <Route path="dashboard" element={<BaseLayout><Dashboard /></BaseLayout>} />
       <Route path="*" element={<Page404/>} />
     </Routes>
   )
